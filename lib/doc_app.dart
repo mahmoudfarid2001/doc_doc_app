d5c68197541d6db5b1e0815e92c78a0f3996b507
Mahmoud Farid <103080766+mahmoudfarid2001@users.noreply.github.com>
2025-04-17 05:52:41 +0200

    no message

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -4,8 +4,7 @@ import 'package:doc_doc_app/core/themeing/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart'; 
 
-class DocApp extends StatelessWidget {
-  
+class DocApp extends StatelessWidget {  
   const DocApp({super.key, required this.appRouter});
   final AppRouter appRouter;
   @override
